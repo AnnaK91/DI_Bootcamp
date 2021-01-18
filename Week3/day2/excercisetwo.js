1e841d239fc8c87428ff753e5c8709172cfd6746
Anna Konig <annna.konig@gmail.com>
2021-01-18 21:23:56 +0200

    excercisetwo

---
+++ Week3/day2/excercisetwo.js
@@ -0,0 +1,20 @@
+var a = ["my","favorite","color","is","blue"];
+var text = a.join(" ");
+console.log(text);
+
+
+
+
+
+// Join	// Join the arrays
+
+// var family = ['Amit', 'Anna', 'Francisco'];
+
+// var familyTwo = ['Isaac', 'Rafi'];
+
+
+//join    // Contrary to split - Show all elements of the array in a text string
+
+// var text = family.join('-');
+
+// console.log(text);
