58ee115310dce3b652cef85fc358e1a22c23ccf8
Anna Konig <annna.konig@gmail.com>
2021-01-18 21:09:16 +0200

    excerciseone

---
+++ Week3/day3/excerciseone.js
@@ -0,0 +1,111 @@
+// 5 + "34"
+
+//let a = 5;
+//let b = "34";
+//let c = a+b;
+//console.log(c);
+
+
+// 5 - "4"
+
+//let a = 5;
+//let b = "4";
+//let c = a-b;
+//console.log(c);
+
+// 10 % 5
+
+//var firstNumber = 10;
+//var secondNumber = 5;
+//var result = firstNumber % secondNumber;
+//console.log(result);
+
+// 5 % 10
+
+//var firstNumber = 5;
+//var secondNumber = 10;
+//var result = firstNumber % secondNumber;
+//console.log(result);
+
+// "Java + Script"
+
+//var text1 = "Java"
+//var text2 = "Script"
+
+//var newtext = text1 + text2
+
+//console.log(newtext);
+
+
+// "" + ''
+
+//var text1 = " "
+//var text2 = " "
+
+//var newtext = text1 + text2;
+//console.log(newtext);
+
+// " " + 0
+
+//var text = " ";
+//var number = 0;
+
+//var newtext = text + number;
+
+//console.log(newtext);
+
+
+// true + true 
+
+//var boolean1 = true;
+
+//var boolean2 = true;
+
+
+//console.log(boolean1 + boolean2);
+
+
+// true + false 
+
+//var boolean1 = true;
+
+//var boolean2 = false;
+
+//console.log(boolean1 + boolean2);
+
+
+//false + true 
+
+//var boolean1 = false;
+
+//var boolean2 = true;
+
+//console.log(boolean1 + boolean2);
+
+
+
+//false - false 
+
+//var boolean1 = false;
+
+//var boolean2 = false;
+
+//console.log(boolean1 - boolean2);
+
+
+// 3 -4
+
+//let a = 3;
+//let b = 4;
+//let c = a-b;
+//console.log(c);
+
+
+
+//"Bob" - "bill"
+
+
+
+//var text = 'Bob';
+//var textReplaced = text.replace('Bob', 'bill');
+//console.log(textReplaced);
