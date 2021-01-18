0b4fba620466d6410f2b238d14736c56e9f1f365
Anna Konig <annna.konig@gmail.com>
2021-01-18 23:27:46 +0200

    update on excercise two

--- Week3/day2/excercisetwo.js
+++ Week3/day2/excercisetwo.js
@@ -1,6 +1,21 @@
-var a = ["my","favorite","color","is","blue"];
-var text = a.join(" ");
-console.log(text);
+// better option: tostring
+
+
+let a = ["my","favorite","color","is","blue"];
+
+a.toString();
+
+console.log(a);
+
+
+
+
+
+
+// version 1
+//var a = ["my","favorite","color","is","blue"];
+//var text = a.join(" ");
+//console.log(text);
 
 
 
