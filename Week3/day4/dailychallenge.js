e1f2c728df5e0b564350305c32dd550c46a9c411
Anna Konig <annna.konig@gmail.com>
2021-01-21 19:39:56 +0200

    daily challenge first try

---
+++ Week3/day4/dailychallenge.js
@@ -0,0 +1,9 @@
+// Instructions
+// Create a variable called sentence. Assign to this variable, a string that has the word “not” and “bad” inside. For example, “The movie is not that bad really, I like it”
+// Create a variable called wordNot that retrieves the first appearance of the substring “not” (from the sentence)
+// Create a variable called wordBad that retrieves the first appearance of the substring “bad” (from the sentence)
+// If the word “bad” follows the word “not”, you should replace the whole “not…bad” substring with “good”, then console.log the result.
+// For example, the result here should be : “The movie is good really, I like it”
+// If the words “not” and “bad” are not in the right sequence (or are not in the sentence), just console.log the original sentence.
+
+var sentence = "The move is not that bad really, I liked it."
