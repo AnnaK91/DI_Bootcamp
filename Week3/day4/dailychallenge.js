74193ef8dbe220a031cf597af16cc18e3208964a
Anna Konig <annna.konig@gmail.com>
2021-01-22 18:51:49 +0200

    daily challenge

--- Week3/day4/dailychallenge.js
+++ Week3/day4/dailychallenge.js
@@ -6,4 +6,33 @@
 // For example, the result here should be : “The movie is good really, I like it”
 // If the words “not” and “bad” are not in the right sequence (or are not in the sentence), just console.log the original sentence.
 
-var sentence = "The move is not that bad really, I liked it."
+//let str = "This dinner is not that bad!";
+let str = "This dinner is not that bad ! You cook well!";
+// //split into chars
+let array = str.split("");
+console.log(array);
+
+// //find index where not starts
+
+let wordNot = str.indexOf("not");
+console.log(wordNot);
+
+// //find index where bad starts
+let wordBad = str.indexOf("bad");
+console.log(wordBad);
+
+let deleteCount = (wordBad - wordNot-2);
+// //if not is before bad, and not and bad exist in the string (-1 is out of range)
+if (wordNot < wordBad && wordNot != -1 && wordBad != -1) {
+
+//take out not and bad, replace it with good
+array.splice(wordNot, deleteCount, "good");
+array.splice(wordNot, wordBad-wordNot-2, "good");
+
+console.log(array)
+
+// 	//join the chars back
+str = array.join("");
+}
+
+console.log(str);
---
+++ Week3/day5/script.js
@@ -0,0 +1,41 @@
+// for (let i= 0; i <=15; i++) {
+//     console.log(i);
+// }
+// if (i === 0) {
+//     console.log(i + "even");
+// }
+
+// if (i % 2 === 0) {
+
+//     console.log(i + "even");
+// }
+
+// else {
+//     console.log( i + "odd");
+// }
+
+
+// for (let i = 0; i <= 15; i++) {
+//     if (i === 0) {
+//         console.log(i + " is even");
+//     }
+
+//     else if (i % 2 === 0) {
+//         console.log(i + " is even");
+//     }
+//     else {
+//         console.log(i + " is odd");
+//     }
+
+// }
+// for (var x=0; x<=15; x++) {
+//     if (x === 0) {
+//             console.log(x +  " is even");
+//     }
+//     else if (x % 2 === 0) {
+//             console.log(x + " is even");   
+//     }
+//     else {
+//             console.log(x + " is odd");
+//     }
+// }
