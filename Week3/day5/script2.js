70afed6f5f429fba99e0873fe7a01df268ea24c5
Anna Konig <annna.konig@gmail.com>
2021-01-22 20:00:48 +0200

    updates on the class excercise

--- Week3/day5/script2.js
+++ Week3/day5/script2.js
@@ -13,10 +13,35 @@
 // if the item is not a string, go out of the loop.
 // if the item is a string, display it.
 
-
-
+//Solution:
 let names = ["john", "sarah", 23, "Rudolf", 34];
 
+//Step 1:
+for (i of names) {
+    if (typeof (i) === 'string') {
 
-for (let i of names) {
-    if (typeof(i) === 'String')
+        //console.log(i, 'is a string');
+
+        if (i[0] == i[0].toUpperCase()) {
+
+            console.log(i);
+
+        } else {
+
+            // concat first letter with the rest 
+            console.log(i[0].toUpperCase() + i.slice(1, i[i.lenghth - 1]));
+        }
+
+    } else {
+        continue;
+    }
+}
+for (i of names) {
+
+    if (typeof (i) === 'string') {
+
+        console.log(i, "is a string");
+    } else {
+        break;
+    }
+}
