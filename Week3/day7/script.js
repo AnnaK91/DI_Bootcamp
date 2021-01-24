518697152ab974b57e9ff36fcf9923cb9c8550b0
Anna Konig <annna.konig@gmail.com>
2021-01-24 20:58:57 +0200

    updates

---
+++ Week3/day7/script.js
@@ -0,0 +1,15 @@
+// excercise 1
+
+
+// var num = 8;
+// var num = 10;
+// console.log(num);
+
+// (Didnt know the answer but I found that it will display 10 - because it will display the last variable.)
+
+
+
+// excercise 2
+
+// Change the code so the var i will be undefined outside of the for loop
+
---
+++ Week4/day1/script.js
@@ -0,0 +1,34 @@
+// Exercise 1
+// 1. Create a structured html file linked to a js file
+// 2. Write a JS function that takes a parameter: myAge
+// 3. Console.log the age of my mum and my dad (my mum is twice my age, and my dad is 1.2 the age of my mum)
+// 4. Call the function
+
+
+// function age(myAge) {
+//     let mom = myAge*2;
+//     let dad = mom*1.2;
+//     console.log("my mom's age is " + mom + " my dad's age is " + dad)
+// }
+
+// age(29)
+
+
+
+// Exercise 2
+// 1. Create a structured html file linked to a js file
+// 2. Write a JS function that takes a parameter: myAge
+// 3. Return the age of my mum (my mum is twice my age)
+// 4. Call the function
+// 5. Console.log the age of my mum
+
+
+function age(myAge) {
+    let mom = myAge * 2;
+
+    return mom;
+}
+console.log(age(29));
+
+
+
