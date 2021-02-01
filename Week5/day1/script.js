be4857aad2b0a3543ac620ae870674cef73481a5
Anna Konig <annna.konig@gmail.com>
2021-02-01 19:22:38 +0200

    class excercises

--- Week5/day1/script.js
+++ Week5/day1/script.js
@@ -1,6 +1,7 @@
-function insert_Row() {
-  var table = document.getElementById("sampleTable");
-  var newRow = document.createElement('tr');
-  newRow.innerHTML = "<td>Row</td><td>Row</td>"
-  table.appendChild(newRow);
-}
+// function insert_Row() {
+//   var table = document.getElementById("sampleTable");
+//   var newRow = document.createElement('tr');
+//   newRow.innerHTML = "<td>Row</td><td>Row</td>"
+//   table.appendChild(newRow);
+// }
+
