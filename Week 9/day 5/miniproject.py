fe3e51e508caf7a6e64456e5470c94d0e26f44b4
Anna Konig <annna.konig@gmail.com>
2021-03-06 19:43:24 +0200

    class

---
+++ Week 9/day 5/miniproject.py
@@ -0,0 +1,13 @@
+class Human:
+    id_number = ""
+    name = ""
+    age 
+    priority = None
+    bood_type = ""
+
+
+
+class Queue:
+    def add_person(self, person)
+        self.person = person
+
