08223123668fa08d210cf5738c2254b311e5b993
Anna Konig <annna.konig@gmail.com>
2021-03-14 21:02:45 +0200

    excercise updates

---
+++ Week 10/day 2/excercise2.py
@@ -0,0 +1,24 @@
+# Create a function that displays the current date.
+# Hint : Use the datetime module
+import datetime
+
+def date_time():
+
+    print(datetime.datetime.now())
+
+
+date_time()
+
+
+# Create a function that displays the amount of time left from now until January 1st.
+# (Example: the 1st January is in 10 days and 10:34:01hours)
+
+
+
+def timeleft_until_January():
+
+    x = datetime.datetime.now()
+    
+
+
+timeleft_until_January()
